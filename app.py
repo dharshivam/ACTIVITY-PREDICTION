1c29e3a4b8cb98d73eecf6822046801ed9c2c69d
dharshivam <shivamdhar8@gmail.com>
2025-08-02 13:33:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
+
+with open ('final_model','rb') as file:
+    model = joblib.load(file)
+
+def prediction(inp_list):
+
+    pred = model.predict([inp_list])[0]
+    if pred == 0:
+        return "Sitting on bed"
+    elif pred == 1:
+        return 'Sitting on chair'
+    elif pred == 2:
+        return 'Lying on bed'
+    else:
+        return "Ambulating"
+    
+
+def main():
+    st.title("ACTIVITY PREDICTION FROM SENSOR DATA")
+    st.subheader('''This application will predict the on going activity of the basis of sensor data provided . Fill the respective fields and it will
+     be prediced.''')
+
+    st.image('imagedep.jpg')
+
+    rfid = st.dropbox("Enter the RFID configuation settings",['Config 1(4 Sensors)','Config 2(3 Sensors)'])
+    st.image('rfidimg.jpg')
+    frid_e = (lambda x:3 if x=='Config2(3 Sensors)'else 4)(rfid)
+
+    ant_ID = st.dropbox('Select the Antenna ID' , [1,2,3,4])
+    rssi = st.text_input('Enter the received signal strength indicator (RSSI)')
+    accv = st.text_input('Enter the vertical acceleration data from sensor')
+    accf = st.text_input('Enter the frontal acceleration data from sensor')
+    accl = st.text_input('Enter the lateral acceleration data from sensor')
+
+    inp_data = [accf , accv ,accl,ant_ID,rssi,rfid_e]
+
+     if st.button('Predict'):
+        response = prediction(inp_data)
+        st.success(response)
+
+if __name__=='__main__':
+    main()
